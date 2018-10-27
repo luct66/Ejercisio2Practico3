63dd06925853dc0498583c81991c46253500e82c
LautaroET <lautaro.online@gmail.com>
2018-10-27 16:38:04 -0300

    interfaz grafica en contrucion

--- src/jFrame/inicio.java
+++ src/jFrame/inicio.java
@@ -30,10 +30,15 @@ public class inicio extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jToggleButton1 = new javax.swing.JToggleButton();
         jToggleButton2 = new javax.swing.JToggleButton();
         jToggleButton3 = new javax.swing.JToggleButton();
         jToggleButton4 = new javax.swing.JToggleButton();
+        jToggleButton5 = new javax.swing.JToggleButton();
+        jToggleButton6 = new javax.swing.JToggleButton();
+        jToggleButton7 = new javax.swing.JToggleButton();
+        jToggleButton8 = new javax.swing.JToggleButton();
+        jToggleButton9 = new javax.swing.JToggleButton();
+        jToggleButton1 = new javax.swing.JToggleButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -45,22 +50,136 @@ public class inicio extends javax.swing.JFrame {
         jLabel1.setText("Sana Sana");
         jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 0, 180, 70));
 
-        jToggleButton1.setText("Pago");
-        jPanel1.add(jToggleButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 220, 150, -1));
+        jToggleButton2.setText("Ingresar Medico");
+        jToggleButton2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jToggleButton2MouseClicked(evt);
+            }
+        });
+        jPanel1.add(jToggleButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 210, 200, -1));
 
-        jToggleButton2.setText("Turnos");
-        jPanel1.add(jToggleButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 260, 150, -1));
+        jToggleButton3.setText("Turno De Trabajo");
+        jToggleButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jToggleButton3ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jToggleButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 250, 200, -1));
 
-        jToggleButton3.setText("Medico");
-        jPanel1.add(jToggleButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 300, 150, -1));
+        jToggleButton4.setText("Mostrar Turno de Trabajo");
+        jToggleButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jToggleButton4ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jToggleButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 450, 200, -1));
 
-        jToggleButton4.setText("Pasiente");
-        jPanel1.add(jToggleButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 340, 150, -1));
+        jToggleButton5.setText("Turno De Pasiente");
+        jToggleButton5.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jToggleButton5MouseClicked(evt);
+            }
+        });
+        jPanel1.add(jToggleButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 290, 200, -1));
+
+        jToggleButton6.setText("Mostrar Pasiente");
+        jToggleButton6.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jToggleButton6ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jToggleButton6, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 330, 200, -1));
+
+        jToggleButton7.setText("Mostrar Medico");
+        jToggleButton7.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jToggleButton7ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jToggleButton7, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 370, 200, -1));
+
+        jToggleButton8.setText("Mostrar Turno de Paiente");
+        jToggleButton8.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jToggleButton8ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jToggleButton8, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 410, 200, -1));
+
+        jToggleButton9.setText("Ingresar Pasiente");
+        jToggleButton9.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jToggleButton9ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jToggleButton9, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 170, 200, -1));
+
+        jToggleButton1.setText("Pagar");
+        jToggleButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jToggleButton1ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jToggleButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 120, 190, -1));
 
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 500, 500));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jToggleButton9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton9ActionPerformed
+        ingresar_paciente a = new ingresar_paciente();
+        a.setVisible(true);
+        dispose();
+    }//GEN-LAST:event_jToggleButton9ActionPerformed
+
+    private void jToggleButton2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jToggleButton2MouseClicked
+        ingresar_medico a = new ingresar_medico();
+        a.setVisible(true);
+        dispose();
+    }//GEN-LAST:event_jToggleButton2MouseClicked
+
+    private void jToggleButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton3ActionPerformed
+        ingresar_turno_trabajo a = new ingresar_turno_trabajo();
+        a.setVisible(true);
+        dispose();
+    }//GEN-LAST:event_jToggleButton3ActionPerformed
+
+    private void jToggleButton5MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jToggleButton5MouseClicked
+        ingresar_turno_paciente a = new ingresar_turno_paciente();
+        a.setVisible(true);
+        dispose();
+    }//GEN-LAST:event_jToggleButton5MouseClicked
+
+    private void jToggleButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton6ActionPerformed
+        mostrar_paciente a = new mostrar_paciente();
+        a.setVisible(true);
+        dispose();
+    }//GEN-LAST:event_jToggleButton6ActionPerformed
+
+    private void jToggleButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton7ActionPerformed
+        mostrar_medico a = new mostrar_medico();
+        a.setVisible(true);
+        dispose();
+    }//GEN-LAST:event_jToggleButton7ActionPerformed
+
+    private void jToggleButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton8ActionPerformed
+        mostrar_turno_paciente a = new mostrar_turno_paciente();
+        a.setVisible(true);
+        dispose();
+    }//GEN-LAST:event_jToggleButton8ActionPerformed
+
+    private void jToggleButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton4ActionPerformed
+        mostrar_turno_trabajo a = new mostrar_turno_trabajo();
+        a.setVisible(true);
+        dispose();
+    }//GEN-LAST:event_jToggleButton4ActionPerformed
+
+    private void jToggleButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton1ActionPerformed
+        pagar a = new pagar();
+        a.setVisible(true);
+        dispose();
+    }//GEN-LAST:event_jToggleButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -104,5 +223,10 @@ public class inicio extends javax.swing.JFrame {
     private javax.swing.JToggleButton jToggleButton2;
     private javax.swing.JToggleButton jToggleButton3;
     private javax.swing.JToggleButton jToggleButton4;
+    private javax.swing.JToggleButton jToggleButton5;
+    private javax.swing.JToggleButton jToggleButton6;
+    private javax.swing.JToggleButton jToggleButton7;
+    private javax.swing.JToggleButton jToggleButton8;
+    private javax.swing.JToggleButton jToggleButton9;
     // End of variables declaration//GEN-END:variables
 }
